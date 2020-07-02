90ea12f3d5e5809d6593a05d32592bfbc4ef9f10
Daniel García <danielgswtu@hotmail.com>
2020-07-02 11:05:31 -0500

    Text change question 1 & 2

--- format/trimestral/trimestral.php
+++ format/trimestral/trimestral.php
@@ -83,7 +83,7 @@
 				<div class="textoPregunta" >
 					<ul>
 						<li style="list-style-type: circle !important">
-							¿Cúal es el número de <strong>denuncias, querellas u otros requisitos</strong> equivalentes que se recibierón en la Fiscalía General de la entidad federativa (incluyendo las recibidas en los Centros de Justicia para Mujeres) durante el año 2020.
+							¿Cuál es el número de <strong>denuncias, querellas u otros requisitos</strong> equivalentes que se recibieron en la Fiscalía General de la entidad federativa (incluyendo las recibidas en los Centros de Justicia para Mujeres) durante el periodo de los meses de <?php echo "$m1, $m2 y $m3" ?> del <?php echo "$anioGlobal"?>.
 						</li>
 					</ul>
 				</div><br><hr><br>
