22472884f774aa7567690cb6e1b45c5521c1d9f9
Luis Enrique Ruiz Martinez <luisenrique-rm@hotmail.com>
2020-07-02 13:56:10 -0500

    getEnlacesByIdUnidad

--- format/trimestral/trimestral.php
+++ format/trimestral/trimestral.php
@@ -41,7 +41,7 @@
 								<td width="50%">
 											<div class="tituloCentralSegu">
 												<div class="titulosCabe1">
-													<label class="titulo1" style="color: #686D72;">REGISTRO DE INDICADORES TRIMESTRALES <? echo $idUnidad; echo $idEnlace; ?></label>
+													<label class="titulo1" style="color: #686D72;">REGISTRO DE INDICADORES TRIMESTRALES</label>
 													<h4> <label id="titfisc" class="titulo2">Dirección de Planeación y Estadística</label></h4>
 												</div>
 											</div>
