fa24ce0f9408babae517840b8ee1668c2d1e1cb5
Luis Enrique Ruiz Martinez <luisenrique-rm@hotmail.com>
2022-03-01 00:58:10 -0600

    hotfix

--- format/mandamientosJudiciales/modalAgraviados_registro.php
+++ format/mandamientosJudiciales/modalAgraviados_registro.php
@@ -11,11 +11,44 @@ $hoy = date("Y-m-d");//Fecha calendario
 if (isset($_POST["idEnlace"])){ $idEnlace = $_POST["idEnlace"]; }
 if (isset($_POST["idUnidad"])){ $idUnidad = $_POST["idUnidad"]; }
 if (isset($_POST["idfisca"])){ $idfisca = $_POST["idfisca"]; }
-
+if (isset($_POST["tipoActualizacion"])){ $tipoActualizacion = $_POST["tipoActualizacion"]; }
 
 if (isset($_POST["tipoModal"])){ $tipoModal = $_POST["tipoModal"]; }
 if (isset($_POST["typeArch"])){ $typeArch = $_POST["typeArch"]; }
 
+if (isset($_POST["ID_MANDAMIENTO_INTERNO"])){ $ID_MANDAMIENTO_INTERNO = $_POST["ID_MANDAMIENTO_INTERNO"]; }
+
+/// SE RECIBE LA INFORMACION DE LA PUESTA A DISPOSICION ///
+if (isset($_POST["dataPrincipalArray"])){
+	$data = json_decode($_POST['dataPrincipalArray'], true);
+	$e = 1;
+}else{ $e = 0; }
+
+			/// SE RECIBE EL ID DEL LA DROGA PARA EDITAR ///
+			if (isset($_POST["ID_DATOS_AGRAVIADO_INTERNO"]) ){ 
+
+				$ID_DATOS_AGRAVIADO_INTERNO = $_POST["ID_DATOS_AGRAVIADO_INTERNO"]; 
+		  //Si se envia ID_DATOS_AGRAVIADO_INTERNO = 0 no es actualizacion, 
+				if($ID_DATOS_AGRAVIADO_INTERNO != 0){
+						$a = 1; // Si a = 1 es actualizacion y se mostraran los datos de la siguiente consulta en este mismo modal
+		    $agraviadosData = get_data_agraviados($conn, $ID_MANDAMIENTO_INTERNO, $ID_DATOS_AGRAVIADO_INTERNO);
+		   	$ID_DATOS_AGRAVIADO_INTERNO = $agraviadosData[0][1];
+						$ID_MANDAMIENTO_INTERNO = $agraviadosData[0][2];
+						$ID_DATOS_AGRAVIADO = $agraviadosData[0][3];
+						$ID_MANDAMIENTO = $agraviadosData[0][4];
+						$NOMBRE = $agraviadosData[0][5];
+						$PATERNO = $agraviadosData[0][6];
+						$MATERNO = $agraviadosData[0][7];
+						$ES_PRINCIPAL = $agraviadosData[0][8];
+		 }else{ 
+		 	//Si $idTrabajoCampo viene con valor 0 no es actualizacion y entra aqui
+		 	$a = 0;
+		 	$ID_DATOS_AGRAVIADO_INTERNO = 0;
+		 }
+		}
+
+
+/*
 //SE RECIBE OBJETO ARRAY CON LOS DATOS PRINCIPALES
 if (isset($_POST["dataPrincipalArray"])){ 
  $data = json_decode($_POST['dataPrincipalArray'], true); 
@@ -49,7 +82,7 @@ if (isset($_POST["ID_MANDAMIENTO_INTERNO"])){
 		$tipoActualizacion = "NO_EXISTE_DATA_AGRAVIADO";
 	}
 }	
-
+*/
 ?>
 
 
@@ -105,7 +138,7 @@ if (isset($_POST["ID_MANDAMIENTO_INTERNO"])){
 <div class="modal-footer">
 	<div class="row">
 		<div class="col-xs-12 col-sm-12  col-md-6 col-md-offset-3">
-			<button type="button" class="btn btn-primary btn-lg" style="width: 100%;" onclick="guardar_mandamiento_agraviado(<? echo $tipoModal; ?>, <? echo $idEnlace; ?>, <?echo $idUnidad; ?> , <?echo $idfisca; ?> , <?echo $ID_MANDAMIENTO_INTERNO; ?> , '<?echo $tipoActualizacion; ?>')" ><span class="glyphicon glyphicon-floppy-disk"></span> Guardar información</button>
+			<button type="button" class="btn btn-primary btn-lg" style="width: 100%;" onclick="guardar_mandamiento_agraviado(<? echo $tipoModal; ?> , <? echo $idEnlace; ?>, <?echo $idUnidad; ?> , <?echo $idfisca; ?> , <? echo $ID_MANDAMIENTO_INTERNO; ?> , <? echo $tipoActualizacion; ?>, <?echo $ID_DATOS_AGRAVIADO_INTERNO; ?>) " ><span class="glyphicon glyphicon-floppy-disk"></span> Guardar información</button>
 		</div>
 		<div class="col-xs-12 col-sm-12  col-md-3">
 			<button type="button" class="btn btn-default btn-lg" data-dismiss="modal" onclick="closeModal_agraviados()" >Cerrar</button>
--- format/mandamientosJudiciales/templates/template_reload_table_mandamientos.php
+++ format/mandamientosJudiciales/templates/template_reload_table_mandamientos.php
@@ -13,13 +13,14 @@ if (isset($_POST["idUnidad"])){ $idUnidad = $_POST["idUnidad"]; }
 
 
 $data = get_data_mandamientos_dia($conn, $idEnlace);
-for ($h=0; $h < sizeof($data) ; $h++) { ?>
+for ($h=0; $h < sizeof($data) ; $h++) {
+	$inculpado = get_data_inculpado($conn, $data[$h][1]); ?>
 <tr>
 							<td><? echo $h+1; ?></td>
 							<td><? echo $data[$h][3]; ?></td>
 							<td><? echo $data[$h][4]; ?></td>
 							<td><? echo $data[$h][5]; ?></td>
-						<!--	<td>sin dato</td>-->
+						 <td><? echo $inculpado[0][5].' '.$inculpado[0][6].' '.$inculpado[0][7]; ?></td>
 							<td><? echo $data[$h][6]; ?></td>
 							<td><? echo $data[$h][7]; ?></td>
 							<td><center><img src="img/editarMandamiento2.png" data-toggle="modal" href="#mandamientos"  onclick="modalMandamientos_registro(0, <? echo $idEnlace; ?>,<? echo $data[$h][1]; ?>,18, 1, <?echo $idfisca; ?>, <?echo $idUnidad; ?>);"><!--<img src="img/resumenMandamiento.png"></center>-->
